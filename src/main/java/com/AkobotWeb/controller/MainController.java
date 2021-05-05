6cf0020139b1ea80b66fc167faec94246b7869e0
junhochoi-git <ajchoi0928@gmail.com>
2021-05-06 02:10:12 +0900

    merge 충돌 대안B

--- src/main/java/com/AkobotWeb/controller/MainController.java
+++ src/main/java/com/AkobotWeb/controller/MainController.java
@@ -7,17 +7,11 @@ package com.AkobotWeb.controller;
 
 import com.AkobotWeb.domain.BoardVO;
 import com.AkobotWeb.service.FirebaseService;
-import lombok.extern.log4j.Log4j;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
-import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/*")
@@ -25,6 +19,20 @@ public class MainController {
     /*TODO Service 작성 */
     @Autowired
     private FirebaseService fbservice;
+
+    /*TODO home.html을 기본페이지? 관습적으로 index.html을 기본페이지로 하는데..*/
+    /* home.html 로그인 하기전 홈 화면(login 버튼 -> 클릭 시 login.html로 이동) */
+   /* @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+    *//* index.html..*/
+
+    /*일단은 /home 요청해서 view 읽도록 함 */
+    @GetMapping("/home")
+    public String home(){
+        return "home";
+    }
 
     /*TODO list페이지 작성*/
     @GetMapping("/tables")
@@ -41,20 +49,15 @@ public class MainController {
         return "questionDetail";
     }
 
-    /*TODO Home.html 로그인 하기전 홈 화면(login 버튼 -> 클릭 시 login.html로 이동) */
-    @GetMapping("/home")
-    public String home(){
-        return "Home";
-    }
     /*TODO Login.html —> bootstrap에 있던 로그인 페이지(일단 그대로 따옴)*/
     @GetMapping("/login")
     public String login(){
         return "login";
     }
-    /*TODO Manual.html —> 아코봇 사용방법이 앞으로 등재될 페이지*/
+    /*TODO manual.html —> 아코봇 사용방법이 앞으로 등재될 페이지*/
     @GetMapping("/manual")
     public String manual(){
-        return "Manual";
+        return "manual";
     }
 
 }
