51580163c58e81954377b958b951d1e5f91e13b5
kwhn <kwhn9486@naver.com>
2021-05-21 23:40:27 +0900

    Topbar에 달력 기능 추가 & 다크모드 버튼 위치 조정

--- src/main/java/com/AkobotWeb/controller/MainController.java
+++ src/main/java/com/AkobotWeb/controller/MainController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
