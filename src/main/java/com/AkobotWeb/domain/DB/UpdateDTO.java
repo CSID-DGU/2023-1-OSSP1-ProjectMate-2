f6bc1c76aeb74790e3ec3be832bb2d68ba646ba6
ChoiJunho <ajchoi0928@gmail.com>
2021-06-13 17:10:40 +0900

    챗봇 DB 관리자 페이지에서 수정 구현

---
+++ src/main/java/com/AkobotWeb/domain/DB/UpdateDTO.java
@@ -0,0 +1,13 @@
+package com.AkobotWeb.domain.DB;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateDTO {
+    private String radio_input;
+    private String Answer;
+}
