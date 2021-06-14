138b93c2807786f5739f87aa9925e55c28973689
junhochoi-git <ajchoi0928@gmail.com>
2021-06-15 05:49:06 +0900

    배포 설정 완료 - 안쓰는 어노테이션 정리

--- src/main/java/com/AkobotWeb/domain/Mail/MailDTO.java
+++ src/main/java/com/AkobotWeb/domain/Mail/MailDTO.java
@@ -4,8 +4,6 @@ package com.AkobotWeb.domain.Mail;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Data
 @AllArgsConstructor
