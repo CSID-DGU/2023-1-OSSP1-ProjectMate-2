09687922e40e77397a8ce61041e7b5c3e1d7822b
Ji soo Jeong <jpy1030@naver.com>
2023-05-30 16:57:41 +0900

    intentmatching third edit

--- Akobot/src/main/resources/chatbot/intentmatching.py
+++ Akobot/src/main/resources/chatbot/intentmatching.py
@@ -3,7 +3,7 @@ import pandas as pd
 import extracting
 
 # load intent data.*will be modified with DB
-data=pd.read_csv("simple.CSV", encoding="utf-8")
+data=pd.read_csv('simple.csv', encoding="utf-8")
 intents=data.iloc[:,0:2]
 
 # preprocess data
@@ -22,28 +22,38 @@ user_keyword={
                 '외국인특별전형':'foreign',
                 '전형요소별평가방법':'test',
                 '전형일정':'schedule',  
-                #level 2
-                '두드림소프트웨어':'susi_dodreamsw',
+                #level 3
+                '두드림소프트웨어':'susi_dodreamsoft',
                 '불교추천':'susi_buddhism',
                 '학교장추천':'susi_principal',
                 '논술':'susi_essay',
                 '두드림':'susi_dodream',
                 '고른기회':'susi_regular',
-                #level 3
-                '일반전형':'jungsi_regular',
+                '특수교육대상자':'susi_special',
+                '재직자':'susi_incumbent',
+                '실기':'susi_performance',
+                #level 4
+                '일반전형':'jungsi_normal',
                 '농어촌전형':'jungsi_farming',
-                '특성화고교':'jungsi_specializedschool',
+                '특성화고교':'jungsi_specialized',
                 '재직자':'jungsi_incumbent',
-                '기초생활수급자및차상위계층':'jungsi_basicliving',
-                #level 4 
-                '농어촌학생재학거주인정기준':'note_farm_standard',
-                '특성화고교졸업자동일계열기준학과':'note_specialmajor',
-                '재직기간산정기준':'note_incumbent_calculating',
-                #level 5
+                '기초생활수급자및차상위계층':'jungsi_basic',
+                #level 6
+                '수시경쟁률':'competition_susi',
+                '정시경쟁률':'competition_jungsi',
+                #level 7
+                '농어촌학생재학거주인정기준':'note_farming',
+                '특성화고교졸업자동일계열기준학과':'note_special',
+                '재직기간산정기준':'note_incumbent',
+                #level 9
                 '서류종합평가':'test_documnet',
                 '면접평가':'test_interview',
                 '학교생활기록부':'test_records',
-                '대학수학능력시험':'test_SAT'
+                '대학수학능력시험':'test_SAT',
+                #level 10
+                '재외국민/외국인':'schedule_foreigner',
+                '정시':'schedule_jungsi',
+                '수시':'schedule_susi'
              }
 
 # raw matching
@@ -51,8 +61,8 @@ matchings=[]
 for keyword in extracting.keywords: #extracted keywords from user input
     for intent_name in intent_names: #intent list from DB
         if intent_name in user_keyword[keyword]: #if extracted keyword contains intent name
-            matchings.append(intent_names) 
-            print(intent_names+ "인텐트로 매칭이 성공했습니다.")
+            matchings.append(intent_name) 
+            print(intent_name + "인텐트로 매칭이 성공했습니다.")
             break
         else:
             matchings.append(["fallback"])
