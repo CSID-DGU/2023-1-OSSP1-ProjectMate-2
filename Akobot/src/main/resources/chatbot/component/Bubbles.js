76ef54ed417853dd1317c4a44da3c577725545dd
박성준 <sungjunp1208@gmail.com>
2023-06-04 15:20:09 +0900

    clear2

--- Akobot/src/main/resources/chatbot/component/Bubbles.js
+++ Akobot/src/main/resources/chatbot/component/Bubbles.js
@@ -81,7 +81,8 @@ function Bubbles(container, self, options) {
     inputWrap.appendChild(inputText)
     inputText.addEventListener("keypress", function(e) {
       // register user input
-      if (e.keyCode == 13) {
+      if (e.keyCode
+         == 13) {
         e.preventDefault()
         typeof bubbleQueue !== false ? clearTimeout(bubbleQueue) : false // allow user to interrupt the bot
         var lastBubble = document.querySelectorAll(".bubble.say")
